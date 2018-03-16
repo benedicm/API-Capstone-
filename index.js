2308813a54a5879dd2e47962bf4f4aeb8c6fba60
Mark Benedict <benedicm@wi.rr.com>
2018-03-16 12:48:53 -0500

    this is a commit message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function findCompletedItems(root) {
 
 	var html = [];
 	html.push(
-		'<table width="100%" border="5" cellspacing="6" cellpadding="3" color="black"><tbody>'
+        '<table width="100%" border="5" cellspacing="6" cellpadding="3" color="black"><tbody>'
 	);
 	for (var i = 0; i < items.length; ++i) {
 		var item = items[i];
@@ -13,6 +13,8 @@ function findCompletedItems(root) {
         var currency = item.sellingStatus[0].currentPrice[0]['@currencyId'];
         var price = item.sellingStatus[0].currentPrice[0].__value__;
         var oldDate = item.listingInfo[0].endTime;
+        var newSellingState = item.listingInfo[0].sellingState;
+        console.log(newSellingState);
         date = new Date(oldDate);
         year = date.getFullYear();
         month = date.getMonth()+1;
@@ -24,10 +26,7 @@ function findCompletedItems(root) {
         month = '0' + month;
         }
         var displayDate = (year+'-' + month + '-'+dt);
-        
-              
-
-		if (null !== title && null !== viewitem) {
+		if (newSellingState ="EndedwithSales" && null !== title && null !== viewitem) {
 			html.push(`<tr><td><img src="${pic}" border="0"></td>
          <td><a href="${viewitem}" target="_blank">${title}</a></td> <td></h3>
         $${price}</td><td> ${displayDate} </td></tr>`);
@@ -45,7 +44,7 @@ function formSubmit(event) {
 		securityAppName: 'MarkBene-CardColl-PRD-b5d8a3c47-2072216b',
 		findBy: 'findCompletedItems',
 	};
-	var url = `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=${filter.findBy}&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${filter.securityAppName}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=findCompletedItems&REST-PAYLOAD&keywords=${term}"card"&paginationInput.entriesPerPage=20`;
+	var url = `http://svcs.ebay.com/services/search/FindingService/v1?OPERATION-NAME=${filter.findBy}&SERVICE-VERSION=1.0.0&SECURITY-APPNAME=${filter.securityAppName}&GLOBAL-ID=EBAY-US&RESPONSE-DATA-FORMAT=JSON&callback=findCompletedItems&REST-PAYLOAD&keywords=${term}"card"&paginationInput.entriesPerPage=80`;
    
     //create <script> element
     //set script element's src attribute
